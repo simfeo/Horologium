ec3e01029ab3478753c722f611695d865e4da83b
simfeo <simidimus@gmail.com>
2025-07-31 22:22:07 +0100

    Upgrading project to sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "sim.horologium.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "sim.horologium.app"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 11
         versionName = "4.1"
 
@@ -34,15 +34,15 @@ android {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
-    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-fragment:2.9.3")
+    implementation("androidx.navigation:navigation-ui:2.9.3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.7.3" apply false
 }
+
+//afterEvaluate {
+//    if (project.hasProperty("android.injected.signing.store.file")) {
+//        println ("key store path: ${project.property("android.injected.signing.store.file")}");
+//    }
+//    if (project.hasProperty("android.injected.signing.store.password")) {
+//        println ("key store password: ${project.property("android.injected.signing.store.password")}");
+//    }
+//    if (project.hasProperty("android.injected.signing.key.alias")) {
+//        println ("key alias: ${project.property("android.injected.signing.key.alias")}");
+//    }
+//    if (project.hasProperty("android.injected.signing.key.password")) {
+//        println ("key password: ${project.property("android.injected.signing.key.password")}");
+//    }
+//}
